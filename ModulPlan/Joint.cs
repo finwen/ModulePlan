5b0f3db9b9f91d3d60eb87600403c555fec3c6e9
Robert Wahlström <finwen_se@hotmail.com>
2017-10-30 10:41:18 +0100

    Test

---
+++ ModulPlan/Joint.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModulPlan
+{
+    public enum JointType
+    {
+        None,
+        Single,
+        Double,
+    }
+
+    public class Joint
+    {
+        JointType type;
+
+        // If different from standard
+        int width;
+        int xOffset;
+        int yOffset;
+        float angle;
+    }
+}
---
+++ NunitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NunitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NunitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e787f53d-16b0-46db-a91d-b2915f563cff")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
